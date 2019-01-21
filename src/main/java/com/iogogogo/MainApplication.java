8d3a6e7961c6eea9632a6b42fe78a511e038eb4f
tao.zeng <tao.zeng@eoitek.com>
2019-01-21 20:25:45 +0800

    init

---
+++ src/main/java/com/iogogogo/MainApplication.java
@@ -0,0 +1,19 @@
+package com.iogogogo;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
+/**
+ * Created by tao.zeng on 2019/1/21.
+ */
+public class MainApplication {
+
+    private final static Logger LOGGER = LoggerFactory.getLogger(MainApplication.class);
+
+    public static void main(String[] args) {
+        LOGGER.info("hello world");
+
+
+    }
+}
